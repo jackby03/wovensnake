34997381064fc9f561bbfa2b0160b540ceb43f75
Jack Del Aguila <70082129+jackby03@users.noreply.github.com>
2026-03-21 13:00:31 -0500

    refactor: Remove global allow(clippy::too_many_arguments) from main.rs (fixes #48)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
     clippy::must_use_candidate,
     clippy::module_name_repetitions,
     clippy::missing_errors_doc,
-    clippy::too_many_arguments,
     clippy::missing_panics_doc,
     clippy::uninlined_format_args
 )]
